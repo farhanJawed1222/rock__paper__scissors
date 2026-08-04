dd108a8e3819629beb9c86b3c21465d385c7bbff
Farhan Jawed <farhanjawed123098@gmail.com>
2026-08-04 21:27:38 +0530

    Fix getHumanChoice function to return invalid choice string instead of undefined

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-function getComputerChoice(){
+function getComputerChoice() {
     // gives random number between 0, 1 and 2
     let random_number = Math.floor(Math.random() * 3);
-    if(random_number == 0){
+    if (random_number == 0) {
         return "Rock";
     }
-    else if(random_number == 1){
+    else if (random_number == 1) {
         return "Paper";
     }
-    else{
+    else {
         return "Scissors";
     }
 }
 
 // function to take human choice
-function getHumanChoice(){
-    let choice = prompt("Enter your choice").toLowerCase();
-    if (choice == "rock" || choice == "paper" || choice == "scissors")
-    {
+function getHumanChoice() {
+    let choice = prompt("Enter choice (rock/paper/scissors)").toLowerCase();
+    if (choice == "rock" || choice == "paper" || choice == "scissors") {
         return choice;
     }
-    else{
+    else {
         return "Invalid choice";
     }
 }
+
