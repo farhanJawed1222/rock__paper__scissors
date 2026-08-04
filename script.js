b9a72ca0aec2e1fa6dc48681d137cfe034e5fe01
Farhan Jawed <farhanjawed123098@gmail.com>
2026-08-04 21:43:48 +0530

    Add playRound function to compare choices and update scores

--- script.js
+++ script.js
@@ -23,3 +23,30 @@ function getHumanChoice() {
     }
 }
 
+function playRound() {
+    // store computer choice that will return from getComputerChoice()
+    const computer_selection = getComputerChoice().toLowerCase();
+    
+    // store human choice that will return from getHumanChoice()
+    const human_selection = getHumanChoice();
+
+    // If human has input invalid choice
+    if (human_selection === "Invalid choice"){
+        return "Invalid choice";
+    }
+    // tie case
+   else if (human_selection === computer_selection) {
+        // no change
+    }
+
+    // check for all the combination in which human will win
+    else if (human_selection === "rock" && computer_selection === "scissors" || human_selection === "paper" && computer_selection === "rock" || human_selection === "scissors" && computer_selection === "paper") {
+        Human_score += 1; 
+    }
+
+    //check for all the combination in which computer will win
+    else {
+        Computer_score += 1;
+    }
+}
+
